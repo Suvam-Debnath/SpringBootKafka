5736d2dfa8274d25c058fd2a95be984d3726a067
Suvam Debnath <debnathsuvam322@gmail.com>
2025-10-06 21:35:09 +0530

    created two mico-services driver and user

---
+++ cab-book-driver/src/main/java/com/suvam/driver/controller/CabLocationController.java
@@ -0,0 +1,4 @@
+package com.suvam.driver.controller;
+
+public class CabLocationController {
+}
---
+++ cab-book-driver/src/test/java/com/suvam/driver/CabBookDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suvam.driver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabBookDriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
